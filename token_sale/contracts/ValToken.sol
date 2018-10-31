090c3c874c59bd2e3bf923335a736408f9714d4e
Sharma-Hrishabh <30hrs1999@gmail.com>
2018-10-31 13:55:01 +0530

    balanceOf added

--- token_sale/contracts/ValToken.sol
+++ token_sale/contracts/ValToken.sol
@@ -4,10 +4,12 @@ contract ValToken{
 //constructor
 //set total tokens
 //read total number of tokens
+mapping(address => uint256) public balanceOf;
   uint256 public totalSupply;
   function ValToken(uint256 _initialSupply) public{
+    balanceOf[msg.sender]=_initialSupply;
     totalSupply = _initialSupply;
-
+    //alloacte the initial supply
   }
 
 
--- token_sale/test/ValToken.js
+++ token_sale/test/ValToken.js
@@ -1,7 +1,7 @@
 var ValToken=artifacts.require("./ValToken.sol");
 
 contract('ValToken',function(accounts){
-
+  var tokenInstance;
  it('sets the total supply on deployment',function(){
 
   return ValToken.deployed().then(function(instance){
@@ -9,6 +9,10 @@ contract('ValToken',function(accounts){
     return tokenInstance.totalSupply();
   }).then(function(totalSupply){
   assert.equal(totalSupply.toNumber(),1000000,'sets the total supplt to 1,000,000 ');
-  });
+  return tokenInstance.balanceOf(accounts[0]);
+}).then(function(adminBalance){
+  assert.equal(adminBalance.toNumber(),1000000,'it allocates initial supply to admin account');
+
 });
-})
+});
+});
